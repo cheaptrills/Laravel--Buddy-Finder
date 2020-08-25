e2e23f216d31df2f988386a390ab35540fc2a8be
cheaptrills <vincevanalphen@yahoo.com>
2020-08-25 23:05:40 +0200

    added navbar components

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <h5 class="my-0 mr-md-auto font-weight-normal">IMD buddy finder</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="/home">home page</a>
+        <a class="p-2 text-dark" href="/user/profile">profiel</a>
+      </nav>
+      <a class="btn btn-outline-primary" href="/user/logout">log uit</a>
+    </div>
